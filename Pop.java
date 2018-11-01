be6586dc2b7a160aeb5b4ca92ef2bd2e0c4eb0df
sicilcollege <44661429+sicilcollege@users.noreply.github.com>
2018-11-01 12:55:39 +0700

    Pop.java

---
+++ Pop.java
@@ -0,0 +1,21 @@
+package com.example.sicillyasjs.latprak;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class Pop extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popup);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.6));
+    }
+}
